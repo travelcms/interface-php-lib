162eb32ef660b686ecabe6c85da95ff72e5b3ccb
l.hanwald <l.hanwald@interactivecms.de>
2010-07-13 15:28:50 +0200

    Applyed zend codingstyle, added coverage xml output to phpunit runner

--- lib/TravelCMSInterface/Backend/Curl.php
+++ lib/TravelCMSInterface/Backend/Curl.php
@@ -9,97 +9,102 @@
  * 
  * @author Leander Hanwald
  */
-class TravelCMSInterface_Backend_Curl implements TravelCMSInterface_Backend_Interface
+class TravelCMSInterface_Backend_Curl 
+  implements TravelCMSInterface_Backend_Interface
 {
-
-  private $host = 'interface.travelcms.de';
-  private $username;
-  private $password;	
-	
-  function __construct() 
-  {
-  	if (!function_exists(curl_init))
-  	  throw new Exception('"curl" extension is missing, but needed for TravelCMS Interface Curl Backend');
-  }  
+    private $_host = 'interface.travelcms.de';
+    private $_username;
+    private $_password; 
   
-  public function setHost($host)
-  {   
-    $this->host = $host;
-  }
+    function __construct() 
+    {
+        if (!function_exists(curl_init))
+            throw new Exception('"curl" extension is missing, but needed for '.
+                                'TravelCMS Interface Curl Backend');
+    }  
   
-  public function setUsername($username)
-  {   
-    $this->username = $username;
-  }
+    public function setHost($host)
+    {   
+        $this->_host = $host;
+    }
   
-  public function setPassword($password)
-  {  
-    $this->password = $password;
-  } 
-
-  public function getRequest($path)
-  {  
-  	$url = sprintf(
-  	    'http://%s:%s@%s%s',
-  	    $this->username,
-  	    $this->password,
-  	    $this->host,
-  	    $path
-  	  );
-  	
-  	$ch = curl_init();
-  	curl_setopt($ch, CURLOPT_URL, $url);
-  	curl_setopt($ch, CURLOPT_HEADER, 0);
-  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  	
-  	return curl_exec($ch);
-  }
-
-  public function postRequest($path, $body)
-  {    
-    $url = sprintf('http://%s%s', $this->host, $path);
-
-    $headers = array(
-      'POST '.$path.' HTTP/1.1',
-      'Host: '.$this->host,
-      'Content-type: application/x-www-form-urlencoded',
-      'Accept: text/xml',
-      'Cache-Control: no-cache',
-      'Pragma: no-cache',      
-      'Content-length: '.strlen($body),
-      'Authorization: Basic '.base64_encode($this->username.':'.$this->password)
-    );       
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-
-    return curl_exec($ch);  	
-  }
-
-  public function deleteRequest($path)
-  {
-    $url = sprintf('http://%s%s', $this->host, $path);
-
-    $headers = array(
-      'Accept: text/xml',
-      'Cache-Control: no-cache',
-      'Pragma: no-cache',      
-      'Authorization: Basic '.base64_encode($this->username.':'.$this->password)
-    );       
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-    
-    return curl_exec($ch);    
-  }  
+    public function setUsername($username)
+    {   
+        $this->_username = $username;
+    }
+  
+    public function setPassword($password)
+    {  
+        $this->_password = $password;
+    } 
+  
+    public function getRequest($path)
+    {  
+        $url = sprintf(
+            'http://%s:%s@%s%s',
+            $this->_username,
+            $this->_password,
+            $this->_host,
+            $path
+        );
+  
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+        return curl_exec($ch);
+    }
+  
+    public function postRequest($path, $body)
+    {    
+        $url = sprintf('http://%s%s', $this->_host, $path);
+  
+        $auth = base64_encode($this->_username.':'.$this->_password);
+        
+        $headers = array(
+            'POST '.$path.' HTTP/1.1',
+            'Host: '.$this->_host,
+            'Content-type: application/x-www-form-urlencoded',
+            'Accept: text/xml',
+            'Cache-Control: no-cache',
+            'Pragma: no-cache',      
+            'Content-length: '.strlen($body),
+            'Authorization: Basic '.$auth
+        );       
+  
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+  
+        return curl_exec($ch);          
+    }
+  
+    public function deleteRequest($path)
+    {
+        $url = sprintf('http://%s%s', $this->_host, $path);
+        
+        $auth = base64_encode($this->_username.':'.$this->_password);
+        
+        $headers = array(
+            'Accept: text/xml',
+            'Cache-Control: no-cache',
+            'Pragma: no-cache',      
+            'Authorization: Basic '.$auth
+        );       
+  
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+      
+        return curl_exec($ch);    
+    }  
   
 }
--- lib/TravelCMSInterface/Backend/FSockOpen.php
+++ lib/TravelCMSInterface/Backend/FSockOpen.php
@@ -9,94 +9,99 @@
  * 
  * @author Leander Hanwald
  */
-class TravelCMSInterface_Backend_FSockOpen implements TravelCMSInterface_Backend_Interface 
+class TravelCMSInterface_Backend_FSockOpen 
+  implements TravelCMSInterface_Backend_Interface
 {
+    private $_host = 'interface.travelcms.de';
+    private $_username;
+    private $_password;  
     
-  private $host = 'interface.travelcms.de';
-  private $username;
-  private $password;  
-  
-  public function setHost($host)
-  {   
-    $this->host = $host;
-  }
-  
-  public function setUsername($username)
-  {   
-  	$this->username = $username;
-  }
-  
-  public function setPassword($password)
-  {  
-  	$this->password = $password;
-  } 
-
-  public function getRequest($path)
-  {     
-    $header =  'GET '.$path.' HTTP/1.0'."\r\n";
-    $header .= 'Host: '.$this->host."\r\n";
-    $header .= 'Content-type: application/x-www-form-urlencoded'."\r\n";
-    $header .= 'Connection: close'."\r\n";
-    $header .= 'Authorization: Basic '.base64_encode($this->username.':'.$this->password)."\r\n";
-    $header .= "\r\n";    
-    
-    return $this->request($this->host, 80, $header);
-  }  
-  
-  public function postRequest($path, $body)
-  { 
-    $header =  'POST '.$path.' HTTP/1.0'."\r\n";
-    $header .= 'Host: '.$this->host."\r\n";
-    $header .= 'Content-type: application/x-www-form-urlencoded'."\r\n";
-    $header .= 'Content-length: '.strlen($body)."\r\n";
-    $header .= 'Connection: close'."\r\n";
-    $header .= 'Authorization: Basic '.base64_encode($this->username.':'.$this->password)."\r\n";
-    $header .= "\r\n";    
-    
-    return $this->request($this->host, 80, $header, $body);	
-  }
-  
-  public function deleteRequest($path)
-  {
-    $header =  'DELETE '.$path.' HTTP/1.0'."\r\n";
-    $header .= 'Host: '.$this->host."\r\n";
-    $header .= 'Content-type: application/x-www-form-urlencoded'."\r\n";
-    $header .= 'Connection: close'."\r\n";
-    $header .= 'Authorization: Basic '.base64_encode($this->username.':'.$this->password)."\r\n";
-    $header .= "\r\n";    
-    
-    return $this->request($this->host, 80, $header);
-  }
-  
-  private function request($host, $port, $header, $body = '')
-  {
-    if (!$connect = fsockopen($host, $port))
-      return false;
-
-    if (fputs($connect, $header.$body) === FALSE) 
-      return false;
-    
-    $resultBody = '';
-    $resultHeader = '';
-    
-    $header = true; 
-    while(!feof($connect))
-    {  
-      $buffer = fgets($connect, 128);
-      
-      if ($header)
-        $resultHeader .= $buffer;
-      else
-        $resultBody .= $buffer;
-      
-      if ($header && $buffer == "\r\n")
-        $header = false;
+    public function setHost($host)
+    {   
+        $this->_host = $host;
     }
     
-    if (fclose($connect) === FALSE)
-      return false;          
-
-    return $resultBody;   	
-  }
+    public function setUsername($username)
+    {   
+        $this->_username = $username;
+    }
+    
+    public function setPassword($password)
+    {  
+        $this->_password = $password;
+    } 
+  
+    public function getRequest($path)
+    {     
+        $auth = base64_encode($this->_username.':'.$this->_password);
+  
+        $header =  'GET '.$path.' HTTP/1.0'."\r\n";
+        $header .= 'Host: '.$this->_host."\r\n";
+        $header .= 'Content-type: application/x-www-form-urlencoded'."\r\n";
+        $header .= 'Connection: close'."\r\n";
+        $header .= 'Authorization: Basic '.$auth."\r\n";
+        $header .= "\r\n";    
+      
+        return $this->request($this->host, 80, $header);
+    }  
+    
+    public function postRequest($path, $body)
+    { 
+        $auth = base64_encode($this->_username.':'.$this->_password);
+  
+        $header =  'POST '.$path.' HTTP/1.0'."\r\n";
+        $header .= 'Host: '.$this->_host."\r\n";
+        $header .= 'Content-type: application/x-www-form-urlencoded'."\r\n";
+        $header .= 'Content-length: '.strlen($body)."\r\n";
+        $header .= 'Connection: close'."\r\n";
+        $header .= 'Authorization: Basic '.$auth."\r\n";
+        $header .= "\r\n";    
+      
+        return $this->request($this->host, 80, $header, $body); 
+    }
+    
+    public function deleteRequest($path)
+    {
+        $auth = base64_encode($this->_username.':'.$this->_password);
+  
+        $header =  'DELETE '.$path.' HTTP/1.0'."\r\n";
+        $header .= 'Host: '.$this->_host."\r\n";
+        $header .= 'Content-type: application/x-www-form-urlencoded'."\r\n";
+        $header .= 'Connection: close'."\r\n";
+        $header .= 'Authorization: Basic '.$auth."\r\n";
+        $header .= "\r\n";    
+      
+        return $this->request($this->host, 80, $header);
+    }
+    
+    private function request($host, $port, $header, $body = '')
+    {
+        if (!$connect = fsockopen($host, $port))
+            return false;
+  
+        if (fputs($connect, $header.$body) === FALSE) 
+            return false;
+      
+        $resultBody = '';
+        $resultHeader = '';
+      
+        $header = true; 
+        while (!feof($connect)) {  
+            $buffer = fgets($connect, 128);
+        
+            if ($header)
+                $resultHeader .= $buffer;
+            else
+                $resultBody .= $buffer;
+        
+            if ($header && $buffer == "\r\n")
+                $header = false;
+        }
+      
+        if (fclose($connect) === FALSE)
+            return false;          
+  
+        return $resultBody;     
+    }
   
 }
